dc97bd9899ab806d2696803a87cebcada852c378
Josh Thompson <thompsonjoshd@gmail.com>
2022-05-16 10:06:12 -0600

    Refactoring! This is looking real good. klass_for

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,35 +6,31 @@ class GildedRose
   attr_reader :name, :days_remaining, :quality
 
   def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+    @item = klass_for(name).new(quality, days_remaining)
   end
 
-  def tick
-    case @name
+  def klass_for(name)
+    case name
     when 'Normal Item'
-      @item = Normal.new(@quality, @days_remaining)
-      @item.tick
+      Normal
     when 'Aged Brie'
-      @item = Brie.new(@quality, @days_remaining)
-      @item.tick
+      Brie
     when 'Sulfuras, Hand of Ragnaros'
-      @item = Sulfuras.new(@quality, @days_remaining)
-      @item.tick
+      Sulfuras
     when 'Backstage passes to a TAFKAL80ETC concert'
-      @item = Backstage.new(@quality, @days_remaining)
-      @item.tick
+      Backstage
     end
   end
 
+  def tick
+    @item.tick
+  end
+
   def quality
-    return @item.quality if @item
-    @quality
+    @item.quality
   end
 
   def days_remaining
-    return @item.days_remaining if @item
-    @days_remaining
+    @item.days_remaining
   end
 end
